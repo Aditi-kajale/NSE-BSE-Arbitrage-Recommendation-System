fbc93c0eafafeb819ad2dbb28122fe578b092a45
Aditi-kajale <aditi1.y.kajale@gmail.com>
2022-09-08 21:30:26 +0530

    pagination added

--- newfrontend/nsebse/src/app/dashboard/dashboard.component.ts
+++ newfrontend/nsebse/src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild} from '@angular/core';
 import { DashboardService } from '../dashboard.service';
 import { LiveStockService } from '../livestockservice.service';
 import {MatPaginator} from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 
 @Component({
@@ -11,21 +12,48 @@ import {MatPaginator} from '@angular/material/paginator';
 })
 export class DashboardComponent implements OnInit { 
   displayedColumns: string[] = ['company_name', 'close_nse', 'close_bse', 'difference', 'percent_diff'];
-  topFive: any;
-  liveStocks: any;
+  datasourceSavedStocks: MatTableDataSource<any> = new MatTableDataSource();
+  datasourceLiveStocks: MatTableDataSource<any> = new MatTableDataSource();
+  datasourceTopFive: MatTableDataSource<any> = new MatTableDataSource();
+
   constructor(private dashboardService: DashboardService, private liveStockService: LiveStockService) { }
 
-  // @ViewChild(MatPaginator) paginator: MatPaginator;
+  savedStocks: any;
+  liveStocks: any;
+  topFive: any;
+
+  @ViewChild('datasourceLiveStocksPaginator')
+  set paginator1(value: MatPaginator) {
+    if (value){
+      this.datasourceLiveStocks.paginator = value;
+    }
+  }
+
+  
+  @ViewChild('datasourceSavedStocksPaginator')
+  set paginator2(value: MatPaginator) {
+    if (value){
+      this.datasourceSavedStocks.paginator = value;
+    }
+  }
 
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.dashboardService.top().subscribe(data => {
       this.topFive = data;
+      this.datasourceTopFive.data = this.topFive;
     });
+
     this.liveStockService.liveStocks().subscribe(data => {
       this.liveStocks = data;
-    });
-    // this.liveStocks.paginator = this.paginator;
+      this.datasourceLiveStocks.data = this.liveStocks;
+      });
+
+
+      this.liveStockService.liveStocks().subscribe(data => {
+        this.savedStocks = data;
+        this.datasourceSavedStocks.data = this.savedStocks;
+        });
   }
 }
 
